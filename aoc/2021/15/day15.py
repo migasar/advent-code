90e6415234edba95252149158ec84a1b71c8527d
Mickael Gaspar <migatsar@gmail.com>
2022-01-22 17:35:01 +0100

    Updated code

--- aoc/2021/15/day15.py
+++ aoc/2021/15/day15.py
@@ -3,6 +3,7 @@
 import pathlib
 import sys
 
+import numpy as np
 
 # Get Input
 INPUT_EX = """1163751742
@@ -24,149 +25,13 @@ puzzle_input = INPUT_EX
 def parse_input(puzzle_input):
     """Parse input"""
 
-    return None
+    data = np.array([
+        [int(x) for x in list(col)] for col in puzzle_input.splitlines()
+    ])
+    return data
 
 
-# Part 1
-# The solution is based on an implementation of the Algorithm of Dijkstra.
-# It builds a search graph to find the shortest path between two nodes.
-
-class Graph():
-
-    def __init__(self, nodes, init_graph):
-        self.nodes = nodes
-        self.graph = self.construct_graph(nodes, init_graph)
-
-
-    def construct_graph(self, nodes, init_graph):
-        """ This methode makes sure that the graph is symmetrical.
-            In other words, if there's a path from node A to B with a value V,
-            there needs to be a path from node B to node A with a value V.
-        """
-
-        graph = {}
-        for node in nodes:
-            graph[node] = {}
-
-        graph.update(init_graph)
-
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-
-        return graph
-
-
-    def get_nodes(self):
-        "Returns the nodes of the graph."
-        return self.nodes
-
-
-    def get_outgoing_edges(self, node):
-        "Returns the neighbours of a node."
-
-        connections = []
-        for out_node in self.nodes:
-            if self.graph[node].get(out_node, False) != False:
-                connections.append(out_node)
-
-        return connections
-
-
-    def value(self, node_1, node_2):
-        "Returns the value of aan edge between two nodes."
-        return self.graph[node_1][node_2]
-
-
-
-
-# Python program for Dijkstra's single
-# source shortest path algorithm. The program is
-# for adjacency matrix representation of the graph
-
-# Library for INT_MAX
-import sys
-
-class Graph():
-
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                    for row in range(vertices)]
-
-    def printSolution(self, dist):
-        print("Vertex tDistance from Source")
-        for node in range(self.V):
-            print(node, "t", dist[node])
-
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
-    def minDistance(self, dist, sptSet):
-
-        # Initialize minimum distance for next node
-        min = sys.maxsize
-
-        # Search not nearest vertex not in the
-        # shortest path tree
-        for v in range(self.V):
-            if dist[v] < min and sptSet[v] == False:
-                min = dist[v]
-                min_index = v
-
-        return min_index
-
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
-    def dijkstra(self, src):
-
-        dist = [sys.maxsize] * self.V
-        dist[src] = 0
-        sptSet = [False] * self.V
-
-        for cout in range(self.V):
-
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
-            u = self.minDistance(dist, sptSet)
-
-            # Put the minimum distance vertex in the
-            # shortest path tree
-            sptSet[u] = True
-
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            for v in range(self.V):
-                if self.graph[u][v] > 0 and
-                sptSet[v] == False and
-                dist[v] > dist[u] + self.graph[u][v]:
-                    dist[v] = dist[u] + self.graph[u][v]
-
-        self.printSolution(dist)
-
-
-# Driver program
-g = Graph(9)
-g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-        [4, 0, 8, 0, 0, 0, 0, 11, 0],
-        [0, 8, 0, 7, 0, 4, 0, 0, 2],
-        [0, 0, 7, 0, 9, 14, 0, 0, 0],
-        [0, 0, 0, 9, 0, 10, 0, 0, 0],
-        [0, 0, 4, 14, 10, 0, 2, 0, 0],
-        [0, 0, 0, 0, 0, 2, 0, 1, 6],
-        [8, 11, 0, 0, 0, 0, 1, 0, 7],
-        [0, 0, 2, 0, 0, 0, 6, 7, 0]
-        ]
-
-g.dijkstra(0)
-
-# This code is contributed by Divyanshu Mehta
-
+data = parse_input(puzzle_input)
 
 
 
